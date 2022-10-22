2e2ddaf8ce01a2dc629be30a796431f4e5984a2a
Nischal Shakya <nischalshakya2015@gmail.com>
2022-10-22 22:50:51 +0545

    refactor(store): external api intergration

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,4 +1,8 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  createSlice,
+  createAsyncThunk,
+} from "@reduxjs/toolkit";
 
 const defaultTasks = [
   { id: "1", title: "Task 1", state: "TASK_INBOX" },
@@ -13,6 +17,19 @@ const TaskBoxData = {
   error: null,
 };
 
+export const fetchTasks = createAsyncThunk("todos/fetchTodos", async () => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos?userId=1"
+  );
+
+  const data = await response.json();
+  return data.map((task) => ({
+    id: task.id,
+    title: task.title,
+    state: task.completed ? "TASK_ARCHIVED" : "TASK_INBOX",
+  }));
+});
+
 const TaskSlice = createSlice({
   name: "taskbox",
   initialState: TaskBoxData,
@@ -24,6 +41,24 @@ const TaskSlice = createSlice({
         state.tasks[task].state = newTaskState;
       }
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchTasks.pending, (state) => {
+        state.status = "pending";
+        state.error = null;
+        state.tasks = [];
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.error = null;
+        state.tasks = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state) => {
+        state.status = "failed";
+        state.error = "Something went wrong";
+        state.tasks = [];
+      });
   },
 });
 
