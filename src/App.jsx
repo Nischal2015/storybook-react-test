75a7324951adc6726986353e0d24183380a8eb03
Nischal Shakya <nischalshakya2015@gmail.com>
2022-10-22 15:05:45 +0545

    Initial setup with storybook

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TaskList from "./components/TaskList";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Title</h1>
+    </div>
+  );
+}
+
+export default App;
