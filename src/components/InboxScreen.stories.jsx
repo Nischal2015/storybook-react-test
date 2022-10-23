382aab8368aeb07d297f3e72ce97aad9b65359fd
Nischal Shakya <nischalshakya2015@gmail.com>
2022-10-23 12:39:22 +0545

    refactor: add todo storybook interactions

--- src/components/InboxScreen.stories.jsx
+++ src/components/InboxScreen.stories.jsx
@@ -4,6 +4,13 @@ import { rest } from "msw";
 import { MockedState } from "./TaskList.stories";
 import { Provider } from "react-redux";
 
+import {
+  fireEvent,
+  within,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@storybook/testing-library";
+
 export default {
   component: InboxScreen,
   title: "InboxScreen",
@@ -24,6 +31,22 @@ Default.parameters = {
       ),
     ],
   },
+};
+
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  // Waits for the element to transition from the loading state
+  await waitForElementToBeRemoved(await canvas.findByTestId("loading"));
+
+  // Waits for the component to be updated based on the store
+  await waitFor(() => {
+    // Simulates pinning the first task
+    fireEvent.click(canvas.getByLabelText("pinTask-1"));
+
+    // Simulate pinning the third task
+    fireEvent.click(canvas.getByLabelText("pinTask-3"));
+  });
 };
 
 export const Error = Template.bind({});
