82f60783fc3aeb4aa9fece052c0783a54094b6dd
Nischal Shakya <nischalshakya2015@gmail.com>
2022-10-24 12:03:50 +0545

    generate storybook-static

---
+++ storybook-static/assets/syntaxhighlighter-74DP7Q2I.e2f78982.js
@@ -0,0 +1,2 @@
+import{b as e,c,v as l}from"./DocsRenderer.eb79da28.js";import"./index.f44dc435.js";import"./iframe.928d792c.js";import"./index.495a17d1.js";import"./index.8e34cd08.js";import"./jsx-runtime.5c9542a3.js";export{e as SyntaxHighlighter,c as createCopyToClipboardFunction,l as default};
+//# sourceMappingURL=syntaxhighlighter-74DP7Q2I.e2f78982.js.map
